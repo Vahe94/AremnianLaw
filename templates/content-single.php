9c182cab92f1492fe1891b9724ddaed94b5bdafe
Smarter1 <vako3008@gmail.com>
2015-05-19 22:34:48 +0400

    Vako

--- templates/content-single.php
+++ templates/content-single.php
@@ -4,7 +4,12 @@
       <h1 class="entry-title"><?php the_title(); ?></h1>
     </header>
     <div class="entry-content">
-      <?php the_content(); ?>
+        <?php
+        if ( has_post_thumbnail() ) {
+            the_post_thumbnail('medium',array('class' => "float-left mr15"));
+        }
+        ?>
+        <?php the_content(); ?>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
