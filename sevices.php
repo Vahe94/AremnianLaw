6b32e63c34d7b2b20fd74004944df6695c18ffdd
Vahe <petrosyan.vahe94@gmail.com>
2015-05-20 16:30:51 +0500

    services

--- sevices.php
+++ sevices.php
@@ -24,13 +24,31 @@ Template Name: services
     ?>
 </div>
 <div class="col-sm-8">
-    <?php $query = new WP_Query(array('category_name' => 'services'));
+    <?php
     $id_get=($_GET["id"]);
-    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();;?>
-    <? echo the_post_thumbnail('thumbnail'); ?>
-    <a href="/services?id=<? echo $post_id ?>"><p class="text-center"> <?php the_title() ?></p></a>
+    if($id_get!="0"){
+        $query = new WP_Query("p=$id_get");
+        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post() ;
+            ?>
+            <div class="col-md-3 col-sm-4 col-xs-6">
+                <div class="caption">
+                    <h2><?php the_content() ?></h2>
+                </div>
+            </div>
+        <?
 
-<?
+        endwhile;
+
+        endif;
+
+        // Reset Query
+
+        //wp_reset_query();
+
+
+    }
+  ?>
+
 
 endwhile;
 
