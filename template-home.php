67bed30e2d1c2aca8429c942bd4eb7b2343cdabc
Smarter1 <vako3008@gmail.com>
2015-05-25 16:14:00 +0400

    Vako

--- template-home.php
+++ template-home.php
@@ -8,8 +8,31 @@ Template Name: Home
 <? the_content(); ?>
 <div class="clear-both"></div>
 <div class="container welcome">
-    <div class="row">
-        <div class="welcome-our-site-div"><span class="welcome-our-site-span">Добро пожаловать на сайт</span></div>
+    <div class="row ">
+        <div class="welcome-our-site-div"><span class="welcome-our-site-span">Наши услуги</span></div>
+        <?php/* dynamic_sidebar('sidebar-footer'); */?>
+        <div id="owl-demo" class="owl-carousel owl-theme">
+            <?php $query = new WP_Query(array('category_name' => 'services'));
+            $k=0;
+            if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();$post_id=get_the_ID();?>
+                <? if($k==0) echo'<div class="item">'; $k++;?>
+                <? echo the_post_thumbnail('medium'); ?>
+                <a href="/services?id=<? echo $post_id ?>"><p class="text-center"> <?php the_title() ?></p></a>
+                <? if($k==2){ echo'</div>'; $k=0;}?>
+            <?
+
+            endwhile;
+
+            endif;
+
+            // Reset Query
+
+            //wp_reset_query();
+            if($k!=0) echo '</div>';
+            ?>
+        </div>
+
+        <div class="welcome-our-site-div"><span class="welcome-our-site-span"><?php echo CFS()->get('slogan'); ?></span></div>
         <div class="row text-center">
             <?php $query = new WP_Query(array('category_name' => 'welcome','posts_per_page' => 4));
 
@@ -21,7 +44,7 @@ Template Name: Home
 
                         <div class="thumb under-thumbnail-margin"> <a href="<? the_permalink(); ?>"><? echo get_the_post_thumbnail($id, 'thumbnail'); ?></a> </div>
                         <div class="caption">
-                            <p><?php echo CFS()->get('welcome_footer'); ?><a href="<? the_permalink(); ?>"><? _e(' (читать дальше...)')?></a></p>
+                            <p><?php echo CFS()->get('welcome_footer'); ?><a href="<? the_permalink(); ?>"><? _e(' (читать далее...)')?></a></p>
                         </div>
                     </div>
                 </div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,128 +1,84 @@
 <footer class="content-info" role="contentinfo">
     <div class="container">
-
-        <div class="row">
+        <div id="publications">
             <?
             $category_id = get_cat_ID( 'publications' );
             $category_link = get_category_link( $category_id );
             ?>
-            <h2 class="publics-title"><hr><a href="<?=$category_link;?>"><span class="left-span">•</span>Публикации<span class="right-span">•</span></a></h2>
-            <div id="publications">
-                <div class="row">
-                    <?php $query = new WP_Query(array('category_name' => 'publications','posts_per_page' => 4));
-                    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-                        <div class="col-md-3 col-sm-4 col-xs-6 posts" onclick="window.location.href='<? the_permalink(); ?>'">
-                            <p class="date"><?php echo get_the_date(); ?></p>
-                            <div class="thumbnail">
-                                <? echo get_the_post_thumbnail($id, 'medium'); ?>
-                            </div>
-                            <p class="description"><?php the_title() ?></p>
+            <h2 class="publics-title">
+                <hr><a><span class="left-span">•</span>Публикации<span class="right-span">•</span></a>
+                <a href="<?=$category_link;?>" class="btn btn-default btn-all-posts btn-xs">Все публикации</a>
+            </h2>
+            <div class="row">
+                <?php $query = new WP_Query(array('category_name' => 'publications','posts_per_page' => 4));
+                if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="col-md-3 col-sm-4 col-xs-6 posts" onclick="window.location.href='<? the_permalink(); ?>'">
+                        <p class="date"><?php echo get_the_date(); ?></p>
+                        <div class="thumbnail">
+                            <? echo get_the_post_thumbnail($id, 'medium'); ?>
                         </div>
-                    <?
-                    endwhile;
-
-                    endif;
-
-                    // Reset Query
-
-                    //wp_reset_query();
-
-                    ?>
-
-                </div>
+                        <p class="description"><?php the_title() ?></p>
+                    </div>
+                <?
+                endwhile;
+                endif;
+                // Reset Query
+                //wp_reset_query();
+                ?>
             </div>
         </div>
-        <div class="row">
+        <div id="publications">
             <?
             $category_id = get_cat_ID( 'news' );
             $category_link = get_category_link( $category_id );
             ?>
-            <h2 class="publics-title"><hr><a href="<?=$category_link;?>"><span class="left-span">•</span>Наши новости<span class="right-span">•</span></a></h2>
-            <div id="publications">
-                <div class="row">
-                    <?php $query = new WP_Query(array('category_name' => 'news','posts_per_page' => 4));
-                    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-                        <div class="col-md-3 col-sm-4 col-xs-6 posts" onclick="window.location.href='<? the_permalink(); ?>'">
-                            <p class="date"><?php echo get_the_date(); ?></p>
-                            <div class="thumbnail">
-                                <? echo get_the_post_thumbnail($id, 'medium'); ?>
-                            </div>
-                            <p class="description"><?php the_title() ?></p>
+            <h2 class="publics-title">
+                <hr><a><span class="left-span">•</span>Наши новости<span class="right-span">•</span></a>
+                <a href="<?=$category_link;?>" class="btn btn-default btn-all-posts btn-xs">Все новости</a>
+            </h2>
+            <div class="row">
+                <?php $query = new WP_Query(array('category_name' => 'news','posts_per_page' => 4));
+                if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="col-md-3 col-sm-4 col-xs-6 posts" onclick="window.location.href='<? the_permalink(); ?>'">
+                        <p class="date"><?php echo get_the_date(); ?></p>
+                        <div class="thumbnail">
+                            <? echo get_the_post_thumbnail($id, 'medium'); ?>
                         </div>
-                    <?
-                    endwhile;
-
-                    endif;
-
-                    // Reset Query
-
-                    //wp_reset_query();
-
-                    ?>
-
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <h2 class="publics-title"><hr><a href="<?=$category_link;?>"><span class="left-span">•</span>Видео<span class="right-span">•</span></a></h2>
-            <div id="videos">
-                <div class="row">
-                    <?php $query = new WP_Query(array('category_name' => 'videos','posts_per_page' => 4));
-
-                    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-                        $post_id=get_the_ID();?>
-                        <div class="col-md-3 col-sm-4 col-xs-6 posts-video">
-                            <p class="date"><?php echo get_the_date(); ?></p>
-                            <div class="thumbnail">
-                                <a href="/video?id=<? echo $post_id ?>" class="video-play">
-                                    <? echo get_the_post_thumbnail($id, 'medium'); ?>
-                                    <!--                                <iframe width="853" height="480" src="--><?php //echo CFS()->get('tipe_video_url_here'); ?><!--" frameborder="0" allowfullscreen></iframe>-->
-                                    <span class="video-play-icon"></span>
-                                </a>
-                            </div>
-                            <p class="description"><?php the_title() ?></p>
-                        </div>
-                    <?
-
-                    endwhile;
-
-                    endif;
-
-                    // Reset Query
-
-                    //wp_reset_query();
-
-                    ?>
-
-                </div>
-            </div>
-        </div>
-        <div class="row ">
-            <div class="welcome-our-site-div"><span class="welcome-our-site-span">Наши услуги</span></div>
-            <?php/* dynamic_sidebar('sidebar-footer'); */?>
-            <div id="owl-demo" class="owl-carousel owl-theme">
-                <?php $query = new WP_Query(array('category_name' => 'services'));
-                $k=0;
-                if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();$post_id=get_the_ID();?>
-                        <? if($k==0) echo'<div class="item">'; $k++;?>
-                        <? echo the_post_thumbnail('medium'); ?>
-                <a href="/services?id=<? echo $post_id ?>"><p class="text-center"> <?php the_title() ?></p></a>
-                <? if($k==2){ echo'</div>'; $k=0;}?>
+                        <p class="description"><?php the_title() ?></p>
+                    </div>
                 <?
-
                 endwhile;
-
                 endif;
-
                 // Reset Query
-
                 //wp_reset_query();
-                if($k!=0) echo '</div>';
                 ?>
             </div>
-
         </div>
-
+        <div id="videos">
+            <h2 class="publics-title"><hr><a href="<?=$category_link;?>"><span class="left-span">•</span>Видео<span class="right-span">•</span></a></h2>
+            <div class="row">
+                <?php $query = new WP_Query(array('category_name' => 'videos','posts_per_page' => 4));
+                if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+                    $post_id=get_the_ID();?>
+                    <div class="col-md-3 col-sm-4 col-xs-6 posts-video">
+                        <p class="date"><?php echo get_the_date(); ?></p>
+                        <div class="thumbnail">
+                            <a href="/video?id=<? echo $post_id ?>" class="video-play">
+                                <? echo get_the_post_thumbnail($id, 'medium'); ?>
+                                <!--                                <iframe width="853" height="480" src="--><?php //echo CFS()->get('tipe_video_url_here'); ?><!--" frameborder="0" allowfullscreen></iframe>-->
+                                <span class="video-play-icon"></span>
+                            </a>
+                        </div>
+                        <p class="description"><?php the_title() ?></p>
+                    </div>
+                <?
+                endwhile;
+                endif;
+                // Reset Query
+                //wp_reset_query();
+                ?>
+            </div>
+        </div>
     </div>
 <!--    <div class="clients">-->
 <!--        <div class="useful">-->
